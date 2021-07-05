0445a51e7cbff5f11419bafd1cffdefaf2c2f68a
R503B <sodanak1n@outlook.com>
2021-07-05 23:48:34 +0200

    Add project files.

---
+++ DemoREDScript/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DemoREDScript
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDY0OTg3QDMxMzkyZTMxMmUzMG5FelNzZG1kRFhpdVptdWk5OStQdFBjZEk5SFZlblBhWmZBamIxNTgzL1k9");
+        }
+    }
+}
